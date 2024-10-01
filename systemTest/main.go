8555af57da533a3a6ee64ff290b0283c844b0b5b
Rajath <bhat.rajath007@gmail.com>
2024-10-01 18:56:52 +0200

    Added test cases

--- systemTest/main.go
+++ systemTest/main.go
@@ -15,70 +15,91 @@ func main() {
 	customComp4 := &CompD{arrBlue: []int{0, 6, 4, 12, 5}, arrGreen: []int{0, 6, 4, 12, 5}, permBlue: []int{4, 0, 2, 3, 1}, permGreen: []int{4, 0, 2, 3, 1}, comp2Ref: customComp2}
 	customComp5 := &CompE{arrBlue: []int{5, 2, 7, 3, 0}, arrGreen: []int{5, 2, 7, 3, 0}, permBlue: []int{4, 3, 0, 2, 1}, permGreen: []int{4, 3, 0, 2, 1}, comp2Ref: customComp2, comp3Ref: customComp3}
 	customComp6 := &CompF{arrBlue: []int{1, 2, 3, 0, 400}, arrGreen: []int{1, 2, 3, 0, 400}, permBlue: []int{3, 0, 2, 1, 4}, permGreen: []int{3, 0, 2, 1, 4}, comp4Ref: customComp4, comp5Ref: customComp5}
-
-	// {18, 165, 35, 81, 2}
-	// {4, 0, 2, 3, 1}
-
-	// {508, 165, 35, 81, 2}
-	// {4, 2, 3, 1, 0}
-
-	// comp4:
-	// 	{
-	// 	}
-	// customComp5 := &Comp5{}
 	compCollec = append(compCollec, customComp1)
 	compCollec = append(compCollec, customComp2)
 	compCollec = append(compCollec, customComp3)
 	compCollec = append(compCollec, customComp4)
 	compCollec = append(compCollec, customComp5)
 	compCollec = append(compCollec, customComp6)
-	// compCollec = append(compCollec, customComp5)
-	components := bg.InitializeComponents("dependency.yaml", compCollec, 3)
+
+	//Uncomment each of the test cases separately and run the code
+	//-----------Test Case 1------------------------------------------------------------------------------
+	components := bg.InitializeComponents("dependency.yaml", compCollec, 1)
+	components.SendReq("CompA", bg.Update, 500, 0)
+	fmt.Println("Reading CompA: ", customComp1.GetLiveData(4))
+	fmt.Println("Reading CompB: ", customComp2.GetLiveData(4))
+	fmt.Println("Reading CompC: ", customComp3.GetLiveData(4))
+	fmt.Println("Reading CompD: ", customComp4.GetLiveData(4))
+	fmt.Println("Reading CompE: ", customComp5.GetLiveData(4))
+	fmt.Println("Reading CompF: ", customComp6.GetLiveData(4))
+	time.Sleep(4 * time.Second)
+	fmt.Println("Reading CompA: ", customComp1.GetLiveData(4))
+	fmt.Println("Reading CompB: ", customComp2.GetLiveData(4))
+	fmt.Println("Reading CompC: ", customComp3.GetLiveData(4))
+	fmt.Println("Reading CompD: ", customComp4.GetLiveData(4))
+	fmt.Println("Reading CompE: ", customComp5.GetLiveData(4))
+	fmt.Println("Reading CompF: ", customComp6.GetLiveData(4))
+	time.Sleep(10 * time.Second)
+
+	//-----------Test Case 2------------------------------------------------------------------------------
+	// components := bg.InitializeComponents("dependency.yaml", compCollec, 2)
+	// components.SendReq("CompA", bg.Update, 500, 0)
+	// fmt.Println("Reading CompA: ", customComp1.GetLiveData(4))
+	// fmt.Println("Reading CompB: ", customComp2.GetLiveData(4))
+	// fmt.Println("Reading CompC: ", customComp3.GetLiveData(4))
+	// fmt.Println("Reading CompD: ", customComp4.GetLiveData(4))
+	// fmt.Println("Reading CompE: ", customComp5.GetLiveData(4))
+	// fmt.Println("Reading CompF: ", customComp6.GetLiveData(4))
+	// time.Sleep(2 * time.Second)
+	// fmt.Println("Reading CompA: ", customComp1.GetLiveData(4))
+	// fmt.Println("Reading CompB: ", customComp2.GetLiveData(4))
+	// fmt.Println("Reading CompC: ", customComp3.GetLiveData(4))
+	// fmt.Println("Reading CompD: ", customComp4.GetLiveData(4))
+	// fmt.Println("Reading CompE: ", customComp5.GetLiveData(4))
+	// fmt.Println("Reading CompF: ", customComp6.GetLiveData(4))
+	// time.Sleep(10 * time.Second)
+
+	//-----------Test Case 3------------------------------------------------------------------------------
+	// components := bg.InitializeComponents("dependency.yaml", compCollec, 3)
+	// components.SendReq("CompB", bg.Update, 100, 0)
+	// components.SendReq("CompA", bg.Update, 500, 2)
+	// components.SendReq("CompF", bg.Update, 55, 3)
+	// time.Sleep(500 * time.Millisecond)
+	// time.Sleep(1 * time.Second)
+	// fmt.Println("Reading CompA: ", customComp1.GetLiveData(4))
+	// fmt.Println("Reading CompB: ", customComp2.GetLiveData(4))
+	// fmt.Println("Reading CompC: ", customComp3.GetLiveData(4))
+	// fmt.Println("Reading CompD: ", customComp4.GetLiveData(4))
+	// fmt.Println("Reading CompE: ", customComp5.GetLiveData(4))
+	// fmt.Println("Reading CompF: ", customComp6.GetLiveData(4))
+	// time.Sleep(2 * time.Second)
+	// fmt.Println("Reading CompA: ", customComp1.GetLiveData(4))
+	// fmt.Println("Reading CompB: ", customComp2.GetLiveData(4))
+	// fmt.Println("Reading CompC: ", customComp3.GetLiveData(4))
+	// fmt.Println("Reading CompD: ", customComp4.GetLiveData(4))
+	// fmt.Println("Reading CompE: ", customComp5.GetLiveData(4))
+	// fmt.Println("Reading CompF: ", customComp6.GetLiveData(4))
+	// time.Sleep(10 * time.Second)
+
+	//-----------Test Case 4------------------------------------------------------------------------------
+	// components := bg.InitializeComponents("dependency.yaml", compCollec, 1)
 
 	// fmt.Println("Initial permutation of CompB:", customComp2.GetStagingDatas())
 	// fmt.Println("Initial permutation of CompD:", customComp4.GetStagingDatas())
 	// fmt.Println("Initial permutation of CompE:", customComp5.GetStagingDatas())
 	// fmt.Println("Initial permutation of CompF:", customComp6.GetStagingDatas())
-	components.SendReq("CompB", bg.Update, 100, 0)
-	components.SendReq("CompA", bg.Update, 500, 2)
-	components.SendReq("CompF", bg.Update, 55, 3)
-	time.Sleep(500 * time.Millisecond)
-
+	// components.SendReq("CompB", bg.Update, 100, 0)
+	// time.Sleep(500 * time.Millisecond)
 	// fmt.Println("Permutation of CompB before cancellation:", customComp2.GetStagingDatas())
 	// fmt.Println("Permutation of CompD before cancellation:", customComp4.GetStagingDatas())
 	// fmt.Println("Permutation of CompE before cancellation:", customComp5.GetStagingDatas())
 	// fmt.Println("Permutation of CompF before cancellation:", customComp6.GetStagingDatas())
-	//components.CancelReq("CompB")
-
-	time.Sleep(1 * time.Second)
+	// components.CancelReq("CompB")
+	// time.Sleep(1 * time.Second)
 	// fmt.Println("Permutation of CompB:", customComp2.GetStagingDatas())
 	// fmt.Println("Permutation of CompD:", customComp4.GetStagingDatas())
 	// fmt.Println("Permutation of CompE:", customComp5.GetStagingDatas())
 	// fmt.Println("Permutation of CompF:", customComp6.GetStagingDatas())
-	// components.SendReq("CompB", bg.Update, 100, 0)
-	// fmt.Println("Permutation of CompA:", customComp1.GetStagingDatas())
-
-	// //time.Sleep(500 * time.Millisecond)
-	// components.CancelReq("CompA")
-	// time.Sleep(500 * time.Millisecond)
-	// fmt.Println("Permutation of CompA:", customComp1.GetStagingDatas())
-
-	fmt.Println("Reading CompA: ", customComp1.GetLiveData(4))
-	fmt.Println("Reading CompB: ", customComp2.GetLiveData(4))
-	fmt.Println("Reading CompC: ", customComp3.GetLiveData(4))
-	fmt.Println("Reading CompD: ", customComp4.GetLiveData(4))
-	fmt.Println("Reading CompE: ", customComp5.GetLiveData(4))
-	fmt.Println("Reading CompF: ", customComp6.GetLiveData(4))
-
-	// components.SendReq("CompD", bg.Update, 500, 0)
-
-	time.Sleep(2 * time.Second)
-	fmt.Println("Reading CompA: ", customComp1.GetLiveData(4))
-	fmt.Println("Reading CompB: ", customComp2.GetLiveData(4))
-	fmt.Println("Reading CompC: ", customComp3.GetLiveData(4))
-	fmt.Println("Reading CompD: ", customComp4.GetLiveData(4))
-	fmt.Println("Reading CompE: ", customComp5.GetLiveData(4))
-	fmt.Println("Reading CompF: ", customComp6.GetLiveData(4))
-	time.Sleep(20 * time.Second)
+	// time.Sleep(10 * time.Second)
 
 }
